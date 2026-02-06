fee19114e2f4e2da41682bf65278e0d3f31e6d67
nterasas <nick.terasas@gmail.com>
2026-02-06 14:05:16 -0700

    Update sales_details.sql

--- scripts/silver/sales_details.sql
+++ scripts/silver/sales_details.sql
@@ -33,13 +33,12 @@ CASE WHEN sls_price IS NULL or sls_price <= 0
 END AS sls_price
 FROM bronze.crm_sales_details
 
+-- =================================================================================== 
 
-
-	
 -- Check for Invalid Dates
 SELECT
 NULLIF(sls_order_dt, 0) sls_order_dt
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_order_dt <= 0 
 OR LEN(sls_order_dt) != 8
 OR sls_order_dt > 20500101
@@ -49,7 +48,7 @@ OR sls_order_dt < 19000101
 
 SELECT
 *
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 
 
@@ -58,20 +57,13 @@ WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 -- >> Values must not be NULL, zero, or negative
 
 SELECT DISTINCT
-sls_sales AS old_sls_sales,
+sls_sales,
 sls_quantity,
-sls_price AS old_sls_price,
-
-CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
-		THEN sls_quantity * ABS(sls_price)
-	 ELSE sls_sales
-END AS sls_sales,
-CASE WHEN sls_price IS NULL or sls_price <= 0
-		THEN sls_sales / NULLIF(sls_quantity, 0)
-	 ELSE sls_price
-END AS sls_price
-FROM bronze.crm_sales_details
+sls_price
+FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
 OR sls_sales IS NULL OR sls_quantity IS NULL or sls_price IS NULL
 OR sls_sales <= 0 OR sls_quantity <= 0 or sls_price <= 0
 ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT * FROM silver.crm_sales_details
