021e941394567a05f88a351809c92dc6ad997b21
nterasas <nick.terasas@gmail.com>
2026-02-06 15:21:21 -0700

    Create erp_loc_a101.sql

---
+++ scripts/silver/erp_loc_a101.sql
@@ -0,0 +1,16 @@
+INSERT INTO silver.erp_loc_a101
+(cid, cntry)
+SELECT
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101 
+
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cntry
+FROM bronze.erp_loc_a101
+ORDER BY cntry
