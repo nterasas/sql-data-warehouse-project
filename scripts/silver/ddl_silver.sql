85e564b01e2078a891667b12038a89921edc9148
nterasas <nick.terasas@gmail.com>
2026-02-06 12:03:13 -0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,71 +1,87 @@
--- Check for Nulls or Duplicates in Primary Key
--- Expectation:  No Result
+/*
+=============================================================================
+DDL Script:  Create Bronze Tables
+=============================================================================
+Script Purpose:
+  This script creates tables in the 'bronze' schema, dropping existing tables
+  if they already exist.
+  Run this script to re-define the DDL structure of 'bronze' Tables
+=============================================================================
+*/
 
-select
-cst_id,
-count(*)
-from silver.crm_cust_info
-group by cst_id
-having count(*) > 1 or cst_id is null
-
-SELECT
-*
-FROM (
-SELECT
-*,
-ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
-FROM silver.crm_cust_info
-)t WHERE flag_last = 1
-
-
--- Check for unwanted Spaces
---Expectation:  No Results
-
-SELECT cst_firstname
-FROM silver.crm_cust_info
-WHERE cst_firstname != TRIM(cst_firstname)
-
-
-
-
--- Transformations
--- Expectations:  Clean data output in cst_firstname and cst_lastname
-INSERT INTO silver.crm_cust_info (
-cst_id,
-cst_key,
-cst_firstname,
-cst_lastname,
-cst_marital_status,
-cst_gndr,
-cst_create_date)
-
-SELECT 
-cst_id,
-cst_key,
-TRIM(cst_firstname) AS cst_firstname,
-TRIM(cst_lastname) AS cst_lastname,
-CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-	 ELSE 'n/a'
-END cst_marital_status,
-CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-	 ELSE 'n/a'
-END cst_gndr,
-cst_create_date
-FROM (
-	SELECT
-	*,
-	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
-	FROM bronze.crm_cust_info
-	WHERE cst_id IS NOT NULL
-)t WHERE flag_last = 1
-
-
--- Data Standarization & Consistency
-
-SELECT DISTINCT cst_gndr
-FROM silver.crm_cust_info
-
-
-SELECT * FROM silver.crm_cust_info
+IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+GO
+  
+CREATE TABLE silver.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+IF OBJECT_ID ('silver.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101 (
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+IF OBJECT_ID ('silver.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12 (
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+IF OBJECT_ID ('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2 (
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
