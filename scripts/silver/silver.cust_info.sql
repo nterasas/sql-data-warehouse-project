f26bb6a67f316d073afaa2f681670d50f008edbe
nterasas <nick.terasas@gmail.com>
2026-02-06 13:00:01 -0700

    Create silver.cust_info.sql

---
+++ scripts/silver/silver.cust_info.sql
@@ -0,0 +1,69 @@
+-- Qualtiy Checks
+-- Check for Nulls or Duplicates in Primary Key
+-- Expectation:  No Result
+
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+-- Check for unwanted Spaces
+-- Expectation:  No Result
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+SELECT cst_marital_status
+FROM bronze.crm_cust_info
+WHERE cst_marital_status != TRIM(cst_marital_status)
+
+
+
+-- Data Consistency
+
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date)
+SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM (
+SELECT
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+FROM bronze.crm_cust_info
+WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
